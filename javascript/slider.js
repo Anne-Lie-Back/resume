02e51a5b298c22212df1ba8f0bcb2da8f2465b3a
Anne-Lie Bäck <a-l.back@outlook.com>
2019-11-19 23:39:31 +0100

    trying to solve problems with responsiveness

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,16 +1,32 @@
 
+/**
+ * Function for slider
+ */
+/**
+ * TOMORROW: Try to put both desktop and mobile hero images in the same div and give the div a class that you tigh to the hidden-class
+ * Then the mediaquiery should do the rest?
+ * 
+ * IT WORKED! Now I can tidy the rest
+ */
 function startSliderDesktop(){
-    let w = window.innerWidth
+    setInterval(sliderImageDesktop, 6000);
+    sliderImageDesktop();
+    /*
+    let w = window.innerWidth || document.documentElement.clientWidth;
+    //window.addEventListener('resize',startSliderDesktop);
     if( w <= 800){
         setInterval(sliderImageMobile, 6000);
+        sliderImageMobile();
     }
     else{
         setInterval(sliderImageDesktop, 6000);
+        sliderImageDesktop();
     }
+    */
 }
 
 function sliderImageDesktop(){
-    changeSliderObject(document.querySelectorAll('.desktopSlider img'), currentIndex);
+    changeSliderObject(document.querySelectorAll('.desktopSlider div'), currentIndex);
     changeSliderObject(document.querySelectorAll('.headlineSlider h4'), currentIndex);
     currentIndex = (currentIndex + 1) % 3;
 }
@@ -21,15 +37,17 @@ function changeSliderObject(sliderObjects, currentIndex) {
     sliderObjects[currentIndex].classList.remove('hidden');
 }
 
-
+/*
 function sliderImageMobile(){
     changeSliderObject(document.querySelectorAll('.mobileSlider img'), currentIndex);
     changeSliderObject(document.querySelectorAll('.headlineSlider h4'), currentIndex);
     currentIndex = (currentIndex + 1) % 3;
 }
 
+
 function changeSliderObject(sliderObjects, currentIndex) {
     sliderObjects[currentIndex].classList.add('hidden');
     currentIndex = (currentIndex + 1) % sliderObjects.length;
     sliderObjects[currentIndex].classList.remove('hidden');
 }
+*/
